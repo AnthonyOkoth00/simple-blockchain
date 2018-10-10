cdcdb60b8bfe9cbb3757aa845ce2f87f56a99d62
Anthony Okoth <misc4tony089@gmail.com>
2018-10-10 22:09:54 +0200

    lottery smart contract

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -3,5 +3,20 @@ const Web3 = require("web3");
 const { interface, bytecode } = require("./compile");
 
 const provider = new HDWalletProvider(
-  "one	hospital diet decade actual wolf core skull inspire silver matrix melody"
+  "one hospital diet decade actual wolf core skull inspire silver matrix melody",
+  "https://rinkeby.infura.io/v3/ee15f176196a44b1a982441ab15348e0"
 );
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy( { data: bytecode, arguments: ["Hi there!"]})
+    .send( { gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+};
+deploy();
